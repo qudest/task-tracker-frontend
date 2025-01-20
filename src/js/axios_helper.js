0e9d7f607b8276321e25dd8e5765a7459c7c9b48
Arsenii Nagornyi <nagornyi.arsenii@mail.ru>
2025-01-20 04:10:06 +0300

    feat: axios helper

---
+++ src/js/axios_helper.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { api_routes } from "./api_routes.js";
+
+export const request = axios.create({
+    baseURL: api_routes.baseURL,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+request.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
