3471320d9222a428e0e2767231513df97151fe7e
Arsenii Nagornyi <nagornyi.arsenii@mail.ru>
2025-01-20 04:09:48 +0300

    feat: add register and user

--- src/js/api_routes.js
+++ src/js/api_routes.js
@@ -3,4 +3,6 @@ const baseURL = 'http://localhost:8080'
 export const api_routes={
     baseURL: baseURL,
     login: baseURL + '/auth/login',
+    register: baseURL + '/auth/register',
+    user: baseURL + '/user'
 }
